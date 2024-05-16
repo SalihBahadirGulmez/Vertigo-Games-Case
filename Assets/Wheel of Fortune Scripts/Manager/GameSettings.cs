942bdf68f2c9fff89419da3b7062e48f877b20de
SalihBahadirGulmez <115195096+SalihBahadirGulmez@users.noreply.github.com>
2024-05-16 04:43:58 +0300

    add: new scriptableObject added

---
+++ Assets/Wheel of Fortune Scripts/Manager/GameSettings.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace WheelOfFortune.Manager.GameManager
+{
+
+    [CreateAssetMenu(menuName = "Wheel of Fortune/Manager/Game Settings")]
+
+    public class GameSettings : ScriptableObject
+    {
+        [SerializeField] private int _superZonePeriod = 30;
+        [SerializeField] private int _safeZonePeriod = 5;
+
+        public int SuperZonePeriod { get { return _superZonePeriod; }}
+        public int SafeZonePeriod { get { return _safeZonePeriod; } }
+    }
+}
